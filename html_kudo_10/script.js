5d91567fda8f81639cee72d458599e6898b6ccde
kdooo <kudoshun.ht@gmail.com>
2021-12-05 16:52:53 +0900

    kadai1

---
+++ html_kudo_10/script.js
@@ -0,0 +1,42 @@
+
+// 動きのきっかけとなるアニメーションの名前を定義
+function fadeAnime(){
+
+    // ふわっ
+    $('.fadeUpTrigger').each(function(){ //fadeUpTriggerというクラス名が
+      var elemPos = $(this).offset().top -50;//要素より、50px上の
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      if (scroll >= elemPos - windowHeight){
+      $(this).addClass('fadeUp');// 画面内に入ったらfadeUpというクラス名を追記
+      }else{
+      $(this).removeClass('fadeUp');// 画面外に出たらfadeUpというクラス名を外す
+      }
+      });
+  }
+  
+// ヘッダーをスクロールした際に色を変える //
+
+  $(function () {
+    $(window).on("scroll", function () {
+      const sliderHeight = $(".mv").height();
+      if (sliderHeight - 30 < $(this).scrollTop()) {
+        $(".js-header").addClass("headerColorScroll");
+      } else {
+        $(".js-header").removeClass("headerColorScroll");
+      }
+    });
+  });
+
+
+  //テキストを含む一般的なモーダル
+$(".info").modaal({
+  overlay_close:true,//モーダル背景クリック時に閉じるか
+  before_open:function(){// モーダルが開く前に行う動作
+    $('html').css('overflow-y','hidden');/*縦スクロールバーを出さない*/
+  },
+  after_close:function(){// モーダルが閉じた後に行う動作
+    $('html').css('overflow-y','scroll');/*縦スクロールバーを出す*/
+  }
+});
+  
